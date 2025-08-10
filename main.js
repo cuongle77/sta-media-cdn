142cd322ab9ac86c5ec2e4b2f98a59a549e59c7e
Cruz Le <xv.Cruz.Le@singtel.com>
2025-08-10 11:35:04 +0700

    Feat: update cdn #7

--- main.js
+++ main.js
@@ -1,4 +1,22 @@
 document.addEventListener("DOMContentLoaded", function () {
+  const lenis = new Lenis({
+    anchors: true,
+  });
+
+  // Synchronize Lenis scrolling with GSAP's ScrollTrigger plugin
+  lenis.on("scroll", ScrollTrigger.update);
+
+  // Add Lenis's requestAnimationFrame (raf) method to GSAP's ticker
+  // This ensures Lenis's smooth scroll animation updates on each GSAP tick
+  gsap.ticker.add((time) => {
+    lenis.raf(time * 1000); // Convert time from seconds to milliseconds
+  });
+
+  // Disable lag smoothing in GSAP to prevent any delay in scroll animations
+  gsap.ticker.lagSmoothing(0);
+
+  /** ===========================================||===================================== */
+
   // Custom mouse cursor animation
   const mouseDot = document.querySelector(".mouse-dot");
 
